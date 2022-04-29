1d898cbe3c91d6a22ff070bf721b9633d37c106b
loremczuk <l.oremczuk@gmail.com>
2022-04-29 20:01:28 +0200

    added PATCH, POST and DELETE methods

--- src/main/java/pl/oremczuk/restapijunit5springsecurityspringdatamysql/EmployeeDataController.java
+++ src/main/java/pl/oremczuk/restapijunit5springsecurityspringdatamysql/EmployeeDataController.java
@@ -1,12 +1,14 @@
 package pl.oremczuk.restapijunit5springsecurityspringdatamysql;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import pl.oremczuk.restapijunit5springsecurityspringdatamysql.models.Employee;
+import pl.oremczuk.restapijunit5springsecurityspringdatamysql.models.LoginCredentials;
 import pl.oremczuk.restapijunit5springsecurityspringdatamysql.repositories.EmployeeRepository;
 import pl.oremczuk.restapijunit5springsecurityspringdatamysql.repositories.LoginCredentialsRepository;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 @RestController
@@ -34,4 +36,49 @@ public class EmployeeDataController {
         return employeeRepository.findFullEmployeeDetails();
 
     }
+
+    @GetMapping("credentials")
+    public List<LoginCredentials> getAllLoginCredentials() {
+
+        return loginCredentialsRepository.findAll();
+
+    }
+
+    @PatchMapping("/{employeeId}/{position}")
+    public ResponseEntity<Employee> updateGivenEmployeePosition(@PathVariable Long employeeId,
+                                                      @PathVariable String position) {
+
+
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(() -> new EntityNotFoundException("Username not found"));
+
+        employee.setPosition(position);
+
+        Employee updatedEmployee = employeeRepository.save(employee);
+
+        return ResponseEntity.ok(employee);
+
+    }
+
+    @PostMapping
+    public ResponseEntity<Employee> addNewEmployee(@RequestBody Employee employee) {
+
+        Employee newEmployee = employeeRepository.save(employee);
+
+        return ResponseEntity.ok(employee);
+
+    }
+
+    @DeleteMapping("/{employeeId}")
+    public ResponseEntity<HttpStatus> deleteGivenEmployee(@PathVariable Long employeeId) {
+
+        Employee employeeToBeDeleted = employeeRepository.findById(employeeId).orElseThrow(() -> new EntityNotFoundException("User not found"));
+
+        employeeRepository.delete(employeeToBeDeleted);
+
+        return ResponseEntity.ok(HttpStatus.OK);
+
+    }
+
+
+
 }
